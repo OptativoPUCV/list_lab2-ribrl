742797f6c161bfe607281565c9b4b62aadd3be88
Replit user <replituser@example.com>
2021-03-31 14:43:38 +0000

    new try

--- list.c
+++ list.c
@@ -114,8 +114,9 @@ void pushCurrent(List * list, const void * data) {
 			list->current->next = newNode;
 			list->tail = newNode;
 		}else{
-      newNode->prev = list->current;
-		  newNode->next = list->current->next;
+      		newNode->prev = list->current;
+			newNode->next = list->current->next;
+			list->current->next = newNode;
       
       //aqui tefalta completar 
 
